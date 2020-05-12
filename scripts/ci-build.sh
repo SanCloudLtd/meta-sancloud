08ec1929b90c1325847554e7ae8fa5134ec491ee
Paul Barker <paul.barker@sancloud.co.uk>
2020-05-12 01:23:54 +0100

    CI: Add GitLab CI config
    Signed-off-by: Paul Barker <paul.barker@sancloud.co.uk>

---
+++ scripts/ci-build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -e
+
+CONF=$1
+shift 1
+IMAGES=$@
+
+( kas shell kas/${CONF}.yml:kas/inc/ci.yml -c "bitbake --setscene-only ${IMAGES}" || true ) | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
+kas shell kas/${CONF}.yml:kas/inc/ci.yml -c "bitbake --skip-setscene ${IMAGES}" | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
+
+mv build/tmp/deploy/images .
