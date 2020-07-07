4bf32883b430ae6cc6613cdc720f6fe559d1f48c
Paul Barker <paul.barker@sancloud.co.uk>
2020-07-07 11:28:44 +0100

    CI: Use latest upstream commits on dunfell
    Signed-off-by: Paul Barker <paul.barker@sancloud.co.uk>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -6,7 +6,7 @@ CONF=$1
 shift 1
 IMAGES=$@
 
-( kas shell kas/${CONF}.yml -c "bitbake --setscene-only ${IMAGES}" || true ) | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
-kas shell kas/${CONF}.yml -c "bitbake --skip-setscene ${IMAGES}" | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
+( kas shell kas/dev/${CONF}.yml -c "bitbake --setscene-only ${IMAGES}" || true ) | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
+kas shell kas/dev/${CONF}.yml -c "bitbake --skip-setscene ${IMAGES}" | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
 
 mv build/tmp/deploy/images .
