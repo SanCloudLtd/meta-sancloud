af080c6e6f84561da5e6692936728a4c858b4549
Paul Barker <paul.barker@sancloud.co.uk>
2020-08-25 16:40:00 +0100

    CI: Simplify kas invocation
    Signed-off-by: Paul Barker <paul.barker@sancloud.co.uk>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -3,10 +3,7 @@
 set -e
 
 CONF=$1
-shift 1
-IMAGES=$@
 
-( kas shell kas/dev/${CONF}.yml -c "bitbake --setscene-only ${IMAGES}" || true ) | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
-kas shell kas/dev/${CONF}.yml -c "bitbake --skip-setscene ${IMAGES}" | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
+kas build kas/dev/${CONF}.yml | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
 
 mv build/tmp/deploy/images .
