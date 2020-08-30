a29d8756a5e126ce7bc471b2db1b04ce3e816b27
Paul Barker <paul.barker@sancloud.co.uk>
2020-08-30 11:22:26 +0100

    CI: Reduce artifacts size & eliminate symlinks
    Signed-off-by: Paul Barker <paul.barker@sancloud.co.uk>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -4,6 +4,40 @@ set -e
 
 CONF=$1
 
+mkdir images
+IMAGES_DIR=`realpath images`
+
 kas build --update --force-checkout kas/dev/${CONF}.yml:kas/inc/ci.yml
 
-mv build/tmp/deploy/images .
+cd build/tmp/deploy/images/bbe
+
+if [[ "$CONF" == "bbe-poky" ]]; then
+    cp  MLO \
+        am335x-sancloud-bbe.dtb \
+        am335x-sancloud-bbe-icu4.dtb \
+        am335x-sancloud-bbei-wifi.dtb  \
+        modules-bbe.tgz \
+        u-boot.img \
+        u-boot-initial-env \
+        zImage \
+        core-image-base-bbe.wic.bmap \
+        core-image-base-bbe.wic.xz \
+        core-image-base.env \
+        ${IMAGES_DIR}
+elif [[ "$CONF" == "bbe-arago" ]]; then
+    cp  MLO \
+        am335x-sancloud-bbe.dtb \
+        am335x-sancloud-bbe-icu4.dtb \
+        am335x-sancloud-bbei-wifi.dtb  \
+        modules-bbe.tgz \
+        u-boot.img \
+        u-boot-initial-env \
+        zImage \
+        tisdk-base-image-bbe.wic.bmap \
+        tisdk-base-image-bbe.wic.xz \
+        tisdk-base-image.env \
+        tisdk-default-image-bbe.wic.bmap \
+        tisdk-default-image-bbe.wic.xz \
+        tisdk-default-image.env \
+        ${IMAGES_DIR}
+fi
