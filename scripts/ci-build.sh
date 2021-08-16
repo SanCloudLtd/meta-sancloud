fa4bc938b945524ecc400b3fbc9664004800ae3f
Paul Barker <paul.barker@sancloud.com>
2021-08-16 09:26:26 +0100

    scripts: Add build path argument to CI script
    Ref: BSP-46
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 BUILD_DISTRO=poky
 BUILD_SDK=no
+BUILD_PATH=build
 KERNEL_PROVIDER=
 SITE_CONF=
 KAS_PREFIX=kas/dev
@@ -12,7 +13,7 @@ KAS_INCLUDES="kas/inc/ci.yml"
 usage() {
     echo "meta-sancloud CI build script"
     echo "Usage:"
-    echo "    $0 [-R] [-A] [-s] [-n] [-k KERNEL] [-i SITE_CONF] [-h]"
+    echo "    $0 [-R] [-A] [-s] [-n] [-k KERNEL] [-i SITE_CONF] [-p BUILD_PATH] [-h]"
     echo ""
     echo "    -R: Build release config (default is development config)."
     echo ""
@@ -25,10 +26,13 @@ usage() {
     echo ""
     echo "    -i SITE_CONF: Use the given file to provide site-specific configuration."
     echo ""
+    echo "    -p BUILD_PATH: Use the given path for all intermediate files."
+    echo "        The default build path is 'build' under the current directory."
+    echo ""
     echo "    -h: Print this help message and exit".
 }
 
-while getopts ":RAsk:i:h" opt; do
+while getopts ":RAsk:i:p:h" opt; do
     case $opt in
         R)
             KAS_PREFIX=kas
@@ -44,6 +48,9 @@ while getopts ":RAsk:i:h" opt; do
             ;;
         i)
             SITE_CONF=`realpath $OPTARG`
+            ;;
+        p)
+            BUILD_PATH="$(realpath "$OPTARG")"
             ;;
         h)
             usage
@@ -69,49 +76,50 @@ if [[ -n "$KERNEL_PROVIDER" ]]; then
     KAS_INCLUDES="kas/inc/kernel-${KERNEL_PROVIDER}.yml:${KAS_INCLUDES}"
 fi
 
-rm -rf images build
-mkdir images build
+rm -rf images "$BUILD_PATH"
+mkdir images "$BUILD_PATH"
+export KAS_BUILD_DIR="$BUILD_PATH"
 
 if [[ -n "$SITE_CONF" ]]; then
     echo ">>> Linking '$SITE_CONF' as site configuration"
-    mkdir build/conf
-    ln -s "$SITE_CONF" build/conf/site.conf
+    mkdir "$BUILD_PATH/conf"
+    ln -s "$SITE_CONF" "$BUILD_PATH/conf/site.conf"
 fi
 
 echo ">>> Building images"
 kas build --update --force-checkout ${KAS_PREFIX}/bbe-${BUILD_DISTRO}.yml:$KAS_INCLUDES
-cp  build/tmp/deploy/images/bbe/MLO \
-    build/tmp/deploy/images/bbe/am335x-sancloud-bbe.dtb \
-    build/tmp/deploy/images/bbe/modules-bbe.tgz \
-    build/tmp/deploy/images/bbe/u-boot.img \
-    build/tmp/deploy/images/bbe/u-boot-initial-env \
-    build/tmp/deploy/images/bbe/zImage \
+cp  "$BUILD_PATH/tmp/deploy/images/bbe/MLO" \
+    "$BUILD_PATH/tmp/deploy/images/bbe/am335x-sancloud-bbe.dtb" \
+    "$BUILD_PATH/tmp/deploy/images/bbe/modules-bbe.tgz" \
+    "$BUILD_PATH/tmp/deploy/images/bbe/u-boot.img" \
+    "$BUILD_PATH/tmp/deploy/images/bbe/u-boot-initial-env" \
+    "$BUILD_PATH/tmp/deploy/images/bbe/zImage" \
     images
 
 for dtb in am335x-sancloud-bbe-icu4.dtb am335x-sancloud-bbei-wifi.dtb am335x-sancloud-bbe-lite.dtb; do
-    srcpath="build/tmp/deploy/images/bbe/${dtb}"
+    srcpath="$BUILD_PATH/tmp/deploy/images/bbe/${dtb}"
     if [[ -e "${srcpath}" ]]; then
         cp "${srcpath}" images
     fi
 done
 
 if [[ "$BUILD_DISTRO" == "poky" ]]; then
-    cp  build/tmp/deploy/images/bbe/core-image-base-bbe.wic.bmap \
-        build/tmp/deploy/images/bbe/core-image-base-bbe.wic.xz \
-        build/tmp/deploy/images/bbe/core-image-base.env \
+    cp  "$BUILD_PATH/tmp/deploy/images/bbe/core-image-base-bbe.wic.bmap" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/core-image-base-bbe.wic.xz" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/core-image-base.env" \
         images
 elif [[ "$BUILD_DISTRO" == "arago" ]]; then
-    cp  build/tmp/deploy/images/bbe/tisdk-base-image-bbe.wic.bmap \
-        build/tmp/deploy/images/bbe/tisdk-base-image-bbe.wic.xz \
-        build/tmp/deploy/images/bbe/tisdk-base-image.env \
-        build/tmp/deploy/images/bbe/tisdk-default-image-bbe.wic.bmap \
-        build/tmp/deploy/images/bbe/tisdk-default-image-bbe.wic.xz \
-        build/tmp/deploy/images/bbe/tisdk-default-image.env \
+    cp  "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-base-image-bbe.wic.bmap" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-base-image-bbe.wic.xz" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-base-image.env" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-default-image-bbe.wic.bmap" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-default-image-bbe.wic.xz" \
+        "$BUILD_PATH/tmp/deploy/images/bbe/tisdk-default-image.env" \
         images
 fi
 
 if [[ "$BUILD_SDK" == "yes" ]]; then
     echo ">>> Building SDK"
     kas build ${KAS_PREFIX}/bbe-sdk-${BUILD_DISTRO}.yml:$KAS_INCLUDES
-    cp build/tmp/deploy/sdk/${BUILD_DISTRO}-*.sh images
+    cp "$BUILD_PATH/tmp/deploy/sdk/${BUILD_DISTRO}-"*.sh images
 fi
