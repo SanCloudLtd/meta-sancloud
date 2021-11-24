cb13ddf92635afa95b22da14c8363e989608831e
Paul Barker <paul.barker@sancloud.com>
2021-11-24 12:15:38 +0000

    Switch back to u-boot script for booting
    It's not possible to automatically detect the partition we're booting
    from and set the kernel command line appropriately when using an
    extlinux.conf file. This means the rootfs image can only support one
    boot medium at a time (SD card) and would need to be modified to boot
    from an alternative medium (eMMC, USB, etc).
    
    To fix this we need to switch back to using a u-boot script which can
    run the `part uuid ...` command. The script is updated as the
    `loadimage` and `loadfdt` variables we previously used no longer exist
    in u-boot v2021.10.
    
    Ref: BSP-70
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

---
+++ recipes-bsp/u-boot/u-boot-script-bbe/boot.cmd
@@ -0,0 +1,18 @@
+# Select MMC partition containing this script
+setenv devtype mmc
+setenv bootpart ${devnum}:${distro_bootpart}
+setenv kernelpath /boot/zImage
+setenv fdtpath /boot/${fdtfile}
+
+# Load kernel & device tree
+echo Loading kernel ${kernelpath}
+load ${devtype} ${bootpart} ${kernel_addr_r} ${kernelpath}
+echo Loading fdt ${fdtpath}
+load ${devtype} ${bootpart} ${fdt_addr_r} ${fdtpath}
+
+# Set kernel arguments
+part uuid ${devtype} ${bootpart} uuid
+setenv bootargs console=${console} root=PARTUUID=${uuid} rw rootwait ${optargs}
+
+# Boot Linux
+bootz ${kernel_addr_r} - ${fdt_addr_r}
