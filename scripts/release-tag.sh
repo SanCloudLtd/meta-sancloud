2519d03357aa44abbf4dfe6ef3b8ef962b539a44
Paul Barker <paul.barker@sancloud.com>
2022-01-27 19:37:50 +0000

    Update release tagging script to work with markdown-extract v2.0.0
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

--- scripts/release-tag.sh
+++ scripts/release-tag.sh
@@ -12,6 +12,6 @@ fi
 VERSION=$1
 
 echo "meta-sancloud ${VERSION}" > tag.txt
-markdown-extract -fri "^${VERSION}" ChangeLog.md | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' >> tag.txt
+markdown-extract -n "^${VERSION}" ChangeLog.md | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' >> tag.txt
 git tag -a -F tag.txt "${VERSION}" HEAD
 rm -f tag.txt
